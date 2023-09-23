6291a485f8907d6ba0d51b029d497c24a5671c54
Zhengrong Wang <seanyukigeek@gmail.com>
2023-09-22 18:53:57 -0700

    Add double bf16 and int8 to mkl gemm

--- src/gemm_cblas.h
+++ src/gemm_cblas.h
@@ -77,5 +77,93 @@ uint64_t dgemm_wrap(int M, int N, int K, void *a, void *b, void *c) {
               (double *)a, lda, (double *)b, ldb, beta, (double *)c, ldc);
 }
 
+#ifdef USE_MKL
+
+// Not sure if these exists in AOCL so limit to MKL.
+void gemm_bf16bf16f32_init(int M, int N, int K, void **a, void **b, void **c) {
+  *a = aligned_alloc(4096, sizeof(MKL_BF16) * M * K);
+  *b = aligned_alloc(4096, sizeof(MKL_BF16) * K * N);
+  *c = aligned_alloc(4096, sizeof(float) * M * N);
+}
+
+uint64_t gemm_bf16bf16f32_wrap(int M, int N, int K, void *a, void *b, void *c) {
+  int lda = K;
+  int ldb = N;
+  int ldc = N;
+  float alpha = 1.0;
+  float beta = 0.0;
+  cblas_gemm_bf16bf16f32(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K,
+                         alpha, (MKL_BF16 *)a, lda, (MKL_BF16 *)b, ldb, beta,
+                         (float *)c, ldc);
+}
+
+void gemm_s8u8s32_init(int M, int N, int K, void **a, void **b, void **c) {
+  *a = aligned_alloc(4096, sizeof(MKL_INT8) * M * K);
+  *b = aligned_alloc(4096, sizeof(MKL_INT8) * K * N);
+  *c = aligned_alloc(4096, sizeof(int32_t) * M * N);
+}
+
+uint64_t gemm_s8u8s32_wrap(int M, int N, int K, void *a, void *b, void *c) {
+  int lda = K;
+  int ldb = N;
+  int ldc = N;
+  MKL_INT8 a_offset = 0;
+  MKL_INT8 b_offset = 0;
+  int32_t c_offset = 0;
+  float alpha = 1.0;
+  float beta = 0.0;
+  cblas_gemm_s8u8s32(CblasRowMajor, CblasNoTrans, CblasNoTrans, CblasFixOffset,
+                     M, N, K, alpha, (MKL_INT8 *)a, lda, a_offset,
+                     (MKL_INT8 *)b, ldb, b_offset, beta, (int32_t *)c, ldc,
+                     &c_offset);
+}
+
+#endif
+
+struct DataTypeConfig {
+  const char *type;
+  wrap_gemm gemm;
+  init_gemm init;
+  int a_size;
+  int b_size;
+  int c_size;
+};
+
+struct DataTypeConfig data_type_configs[] = {
+    {
+        .type = "float",
+        .gemm = sgemm_wrap,
+        .init = sgemm_init,
+        .a_size = sizeof(float),
+        .b_size = sizeof(float),
+        .c_size = sizeof(float),
+    },
+    {
+        .type = "double",
+        .gemm = dgemm_wrap,
+        .init = dgemm_init,
+        .a_size = sizeof(double),
+        .b_size = sizeof(double),
+        .c_size = sizeof(double),
+    },
+#ifdef USE_MKL
+    {
+        .type = "bf16bf16f32",
+        .gemm = gemm_bf16bf16f32_wrap,
+        .init = gemm_bf16bf16f32_init,
+        .a_size = sizeof(MKL_BF16),
+        .b_size = sizeof(MKL_BF16),
+        .c_size = sizeof(float),
+    },
+    {
+        .type = "s8u8s32",
+        .gemm = gemm_s8u8s32_wrap,
+        .init = gemm_s8u8s32_init,
+        .a_size = sizeof(MKL_INT8),
+        .b_size = sizeof(MKL_INT8),
+        .c_size = sizeof(int32_t),
+    },
+#endif
+};
 
 #endif
